a1c18f2856e00b6d4415651b19f7165cfb296afb
sergey <smvienna@yahoo.com>
2020-05-07 17:37:24 +0200

    Lesson 1 completed

--- src/main/java/com/balthazar/taxiBusinessSimulator/Taxi.java
+++ src/main/java/com/balthazar/taxiBusinessSimulator/Taxi.java
@@ -1,5 +1,42 @@
 package com.balthazar.taxiBusinessSimulator;
 
+/**
+ * Klasse Taxi: Zur Erzeugung eines Taxi Objekts
+ */
 public class Taxi {
+    private int sitzplaetze;
+    private double maxTankinhalt;
+    private double aktTankinhalt;
+    private double verbrPro100km;
+    private double kmStand;
 
+    public Taxi(int sitzplaetze, double maxTankinhalt, double aktTankinhalt, double verbrPro100km,
+                double kmStand){
+
+        this.sitzplaetze=sitzplaetze;
+        this.maxTankinhalt=maxTankinhalt;
+        this.aktTankinhalt=aktTankinhalt;
+        this.verbrPro100km=verbrPro100km;
+        this.kmStand=kmStand;
+    }
+
+    public double getMaxTankinhalt() {
+        return maxTankinhalt;
+    }
+
+    public double getAktTankinhalt() {
+        return aktTankinhalt;
+    }
+
+    public double getVerbrPro100km() {
+        return verbrPro100km;
+    }
+
+    public double getKmStand() {
+        return kmStand;
+    }
+
+    public int getSitzplaetze() {
+        return sitzplaetze;
+    }
 }
