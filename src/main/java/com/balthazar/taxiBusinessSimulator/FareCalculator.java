de1f197eaa08d4433c7407bddad20302a6f5457c
Michael <michael.herler@gmx.at>
2020-05-08 22:30:14 +0200

    Lesson 2 created

--- src/main/java/com/balthazar/taxiBusinessSimulator/FareCalculator.java
+++ src/main/java/com/balthazar/taxiBusinessSimulator/FareCalculator.java
@@ -1,21 +1,25 @@
 package com.balthazar.taxiBusinessSimulator;
 
-import java.time.LocalDateTime;
-
 /**
  * Fahrpreismodell:
  * die ersten 30 Kilometer je 80 Cent pro km
  * von Kilometer 30-100 je 70 Cent pro km
  * von Kilometer 100 weg je 60 Cent pro Kilometer
- * wenn der Fahrtantritt nach 20 Uhr und vor 6 Uhr erfolgt soll pauschal 5 € hinzugerechnet werden
- * wenn der Fahrtantritt nach 22 Uhr und vor 5 Uhr erfolgt soll der Kilometer bezogene Tarif um 10% erhöht werden.
  */
 public class FareCalculator {
 
-    public double calculateFare(LocalDateTime startTime, double drivingDistance){
-        return drivingDistance*0.80;
-        // TODO: 06.05.2020  write a method that passes the tests
-        //return 0.0;
+    private double shortRangeLimit=30.0;
+    private double mediumRangeLimit=100.0;
+
+    public double calculateFare(double drivingDistance){
+       if (drivingDistance<shortRangeLimit){
+           return drivingDistance*0.8;
+       }
+       if (drivingDistance<mediumRangeLimit){
+           return 30*0.8 +(drivingDistance-shortRangeLimit)*0.7;
+       }
+       return 30*0.8 + 70*0.7+(drivingDistance-mediumRangeLimit)*0.6;
+
     }
 
 }
--- src/test/java/com/balthazar/taxiBusinessSimulator/FareCalculatorTest.java
+++ src/test/java/com/balthazar/taxiBusinessSimulator/FareCalculatorTest.java
@@ -3,39 +3,22 @@ package com.balthazar.taxiBusinessSimulator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class FareCalculatorTest {
-
-    public static DateTimeFormatter dateTimeFormatter= DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     @Test
     void calculateFare_shortDistance() {
         FareCalculator fareCalculator= new FareCalculator();
-        Assertions.assertEquals(16, fareCalculator.calculateFare(LocalDateTime.parse("2020-05-09 12:15", dateTimeFormatter),
-                20), 0.01, "kurze Fahrt unter 30km");
+        Assertions.assertEquals(16, fareCalculator.calculateFare(20), 0.01, "kurze Fahrt von 20km (20*0.8= 16)");
     }
     @Test
     void calculateFare_mediumDistance() {
         FareCalculator fareCalculator= new FareCalculator();
-        Assertions.assertEquals(59, fareCalculator.calculateFare(LocalDateTime.parse("2020-05-09 12:15", dateTimeFormatter),
-                80), 0.01, "Fahrt zwischen 30 und 100km");
+        Assertions.assertEquals(59, fareCalculator.calculateFare(80), 0.01, "Fahrt zwischen 30 und 100km (30*0.8+ 50*0.7= 59)");
     }
     @Test
     void calculateFare_longDistance() {
         FareCalculator fareCalculator= new FareCalculator();
-        Assertions.assertEquals(16, fareCalculator.calculateFare(LocalDateTime.parse("2020-05-09 12:15", dateTimeFormatter),
-                20), 0.01, "Fahrt über 100 km");
-        Assertions.assertEquals(16, fareCalculator.calculateFare(LocalDateTime.parse("2020-05-09 12:15", dateTimeFormatter),
-                20), 0.01, "kurze Fahrt unter 30km");
-    }
-
-    @Test
-    void calculateFare_longDistance_night() {
-
+        Assertions.assertEquals(79, fareCalculator.calculateFare(110), 0.01, "Fahrt über 110 km (30*0.8+ 70*0.7 + 10*0.6= 79)");
     }
 
 }
